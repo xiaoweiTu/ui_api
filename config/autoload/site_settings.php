31db11cf47b4fc340a872d7575532447f6beefeb
xiaowei <13177839316@163.com>
2020-04-09 16:37:48 +0800

    添加配置

--- config/autoload/site_settings.php
+++ config/autoload/site_settings.php
@@ -7,10 +7,13 @@
  */
 
 return [
-    'web_name'    => 'FastBlog',
-    'web_author'  => 'FastBlog',
+    'web_name'    => '小圆圆伍',
+    'web_author'  => '小圆圆伍',
     'web_desc'    => 'a real FastBlog',
-    'web_keyword' => 'laravel,php,hyperf,技术教程,Mysql,Redis,Linux,CentOs',
+    'web_keyword' => 'UI设计,平面设计,网页设计',
     'web_icon'    => 'FastBlog',
     'web_record'  => 'FastBlog',
+    'web_word'    => 'KEEP THE HAPPY',
+    'web_email'   => 'xiaoyuanyuanwu@126.com',
+    'web_url'     => 'xiaoyuanyuanwu.sanhai.cool',
 ];
